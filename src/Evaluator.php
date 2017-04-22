091f3d72edcba5fd6f99f53ff3e633c0175f6a61
Scotty Christianson <code.archer70@gmail.com>
2017-04-22 17:02:36 +0000

    Some eval refactoring.

--- src/Evaluator.php
+++ src/Evaluator.php
@@ -1,18 +1,11 @@
 <?php
 namespace Desmond;
+use Desmond\functions\Core;
 use Desmond\data_types\IntegerType;
 use Desmond\data_types\TrueType;
 
 class Evaluator
 {
-    private static $FUNCTIONS = [
-        '+' => 'Desmond\addition',
-        '-' => 'Desmond\subtraction',
-        '*' => 'Desmond\multiplication',
-        '/' => 'Desmond\division',
-        'print' => 'Desmond\desmondPrint'
-    ];
-
     public function getReturn($ast)
     {
         if (!is_array($ast)) {
@@ -25,53 +18,7 @@ class Evaluator
                     $ast[$formIndex] = $this->getReturn($atom);
                 }
             }
-
-            return self::$FUNCTIONS[$function->name()]($ast);
+            return CORE::run($function->name(), $ast);
         }
     }
-}
-
-function addition($args)
-{
-    $value = 0;
-    foreach ($args as $arg) {
-        $value += $arg->value();
-    }
-    return new IntegerType($value);
-}
-
-function subtraction($args)
-{
-    $value = $args[0]->value();
-    array_shift($args);
-    foreach ($args as $number) {
-        $value -= $number->value();
-    }
-    return new IntegerType($value);
-}
-
-function multiplication($args)
-{
-    $value = $args[0]->value();
-    array_shift($args);
-    foreach ($args as $number) {
-        $value *= $number->value();
-    }
-    return new IntegerType($value);
-}
-
-function division($args)
-{
-    $value = $args[0]->value();
-    array_shift($args);
-    foreach ($args as $number) {
-        $value /= $number->value();
-    }
-    return new IntegerType($value);
-}
-
-function desmondPrint($string)
-{
-    print($string[0]->value());
-    return new TrueType(true);
 }
---
+++ src/functions/Core.php
@@ -0,0 +1,67 @@
+<?php
+namespace Desmond\functions;
+use Desmond\data_types\IntegerType;
+use Desmond\data_types\TrueType;
+
+class Core
+{
+    private static $FUNCTION_LIST = [
+        '+' => 'addition',
+        '-' => 'subtraction',
+        '*' => 'multiplication',
+        '/' => 'division',
+        'print' => 'outputPrint'
+    ];
+
+    public static function run($func, $args)
+    {
+        if (array_key_exists($func, self::$FUNCTION_LIST)) {
+            return self::{self::$FUNCTION_LIST[$func]}($args);
+        }
+    }
+
+    private static function addition($args)
+    {
+        $value = 0;
+        foreach ($args as $arg) {
+            $value += $arg->value();
+        }
+        return new IntegerType($value);
+    }
+
+    private static function subtraction($args)
+    {
+        $value = $args[0]->value();
+        array_shift($args);
+        foreach ($args as $number) {
+            $value -= $number->value();
+        }
+        return new IntegerType($value);
+    }
+
+    private static function multiplication($args)
+    {
+        $value = $args[0]->value();
+        array_shift($args);
+        foreach ($args as $number) {
+            $value *= $number->value();
+        }
+        return new IntegerType($value);
+    }
+
+    private static function division($args)
+    {
+        $value = $args[0]->value();
+        array_shift($args);
+        foreach ($args as $number) {
+            $value /= $number->value();
+        }
+        return new IntegerType($value);
+    }
+
+    private static function outputPrint($string)
+    {
+        print($string[0]->value());
+        return new TrueType(true);
+    }
+}
