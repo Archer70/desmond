da32aab0c1839d3d1bc15c4e9fe582eccc54afe1
Scotty Christianson <code.archer70@gmail.com>
2017-05-01 18:11:52 +0000

    Refactor evaluator tests.

--- test/EvaluatorTest.php
+++ test/EvaluatorTest.php
@@ -16,185 +16,161 @@ class EvaluatorTest extends TestCase
 
     public function testReturnsAtom()
     {
-        $ast = $this->lexer->readString('5');
-        $this->assertEquals(5, $this->eval->getReturn($ast)->value());
+        $this->assertEquals(5, $this->valueOf('5'));
     }
 
     public function testNestedForms()
     {
-        $ast = $this->lexer->readString('(+ 1 2 (+ 3 4))'); // (+ 3 7)
-        $this->assertEquals(10, $this->eval->getReturn($ast)->value());
-
-        $ast = $this->lexer->readString('(+ 1 2 (+ 3 4) (- 6 2))'); // (+ 3 7 4)
-        $this->assertEquals(14, $this->eval->getReturn($ast)->value());
+        $this->assertEquals(10, $this->valueOf('(+ 1 2 (+ 3 4))'));
+        $this->assertEquals(14, $this->valueOf('(+ 1 2 (+ 3 4) (- 6 2))'));
     }
 
     public function testVector()
     {
-        $ast = $this->lexer->readString('[1 2]');
-        $this->assertInstanceOf('Desmond\\data_types\\VectorType', $this->eval->getReturn($ast));
-        $this->assertEquals(2, $this->eval->getReturn($ast)->get(1)->value());
+        $vector = $this->resultOf('[1 2]');
+        $this->assertInstanceOf('Desmond\\data_types\\VectorType', $vector);
+        $this->assertEquals(2, $vector->get(1)->value());
     }
 
     public function testVectorEvaluatesForms()
     {
-        $ast = $this->lexer->readString('[(+ 2 3)]');
-        $vector = $this->eval->getReturn($ast);
+        $vector = $this->resultOf('[(+ 2 3)]');
         $this->assertEquals(5, $vector->get(0)->value());
     }
 
     public function testVectorEvaluatesSymbols()
     {
-        $ast = $this->lexer->readString('(do (define x 2) [1 x])');
-        $vector = $this->eval->getReturn($ast);
+        $vector = $this->resultOf('(do (define x 2) [1 x])');
         $this->assertEquals(2, $vector->get(1)->value());
     }
 
     public function testHash()
     {
-        $ast = $this->lexer->readString('{:key 1}');
-        $hash = $this->eval->getReturn($ast);
+        $hash = $this->resultOf('{:key 1}');
         $this->assertInstanceOf('Desmond\\data_types\\HashType', $hash);
         $this->assertEquals(1, $hash->get(':key')->value());
     }
 
     public function testHashEvaluatesForms()
     {
-        $ast = $this->lexer->readString('{:key (+ 1 2)}');
-        $hash = $this->eval->getReturn($ast);
+        $hash = $this->resultOf('{:key (+ 1 2)}');
         $this->assertInstanceOf('Desmond\\data_types\\IntegerType', $hash->get(':key'));
         $this->assertEquals(3, $hash->get(':key')->value());
     }
 
     public function testDefine()
     {
-        $ast = $this->lexer->readString('(define my-sym "it worked")');
-        $this->eval->getReturn($ast);
-        $ast = $this->lexer->readString('my-sym');
-        $this->assertEquals('it worked', $this->eval->getReturn($ast)->value());
+        $this->assertEquals('it worked', $this->valueOf('(define my-sym "it worked")'));
     }
 
     public function testDefineCondition()
     {
-        $ast = $this->lexer->readString('(define :hello (if true "world" "universe"))');
-        $this->eval->getReturn($ast);
-        $ast = $this->lexer->readString(':hello');
-        $result = $this->eval->getReturn($ast);
+        $result = $this->resultOf('(define :hello (if true "world" "universe"))');
         $this->assertInstanceOf('Desmond\\data_types\\StringType', $result);
         $this->assertEquals('world', $result->value());
     }
 
     public function testDefinedSymInForm()
     {
-        $ast = $this->lexer->readString('(define :five 5)');
-        $this->eval->getReturn($ast);
-        $ast = $this->lexer->readString('(+ 1 :five)');
-        $this->assertEquals(6, $this->eval->getReturn($ast)->value());
+        $this->assertEquals(
+            6, $this->valueOf('(do (define :five 5) (+ 1 :five))'));
     }
 
     public function testLet()
     {
-        $ast = $this->lexer->readString('(let {:num 5} :num)');
-        $letBlock = $this->eval->getReturn($ast);
-        $this->assertInstanceOf('Desmond\\data_types\\IntegerType', $letBlock);
-        $this->assertEquals(5, $letBlock->value());
+        $this->assertInstanceOf(
+            'Desmond\\data_types\\IntegerType', $this->resultOf('(let {:num 5} :num)'));
+        $this->assertEquals(
+            5, $this->valueOf('(let {:num 5} :num)'));
     }
 
     public function testLetIsOwnEnvironment()
     {
-        $ast = $this->lexer->readString('(let {:num 5} :num)');
-        $this->eval->getReturn($ast);
-        $ast = $this->lexer->readString(':num');
-        $this->assertNotEquals(5, $this->eval->getReturn($ast)->value());
+        $this->assertNotEquals(
+            5, $this->valueOf('(do (let {:num 5} :num) :num)'));
     }
 
     public function testDo()
     {
-        $ast = $this->lexer->readString('(do (define :five 5) (+ 1 :five))');
-        $do = $this->eval->getReturn($ast);
-        $this->assertEquals(6, $do->value());
+        $this->assertEquals(
+            6, $this->valueof('(do (define :five 5) (+ 1 :five))'));
     }
 
     public function testIf()
     {
-        $ast = $this->lexer->readString('(if true "yay")');
-        $result = $this->eval->getReturn($ast);
-        $this->assertEquals('yay', $result->value());
-
-        $ast = $this->lexer->readString('(if false "nope")');
-        $result = $this->eval->getReturn($ast);
-        $this->assertNull($result->value());
+        $this->assertEquals('yay', $this->valueOf('(if true "yay")'));
+        $this->assertNull($this->valueOf('(if false "nope")'));
     }
 
     public function testIfElse()
     {
-        $ast = $this->lexer->readString('(if false "yep" "nope")');
-        $result = $this->eval->getReturn($ast);
-        $this->assertEquals('nope', $result->value());
+        $this->assertEquals(
+            'nope', $this->valueOf('(if false "yep" "nope")'));
     }
 
     public function testLamda()
     {
-        $ast = $this->lexer->readString('(lambda [arg] arg)');
-        $result = $this->eval->getReturn($ast);
-        $this->assertInstanceOf('Desmond\\data_types\\LambdaType', $result);
+        $this->assertInstanceOf(
+            'Desmond\\data_types\\LambdaType', $this->resultOf('(lambda [arg] arg)'));
     }
 
     public function testEvalutationOfLambda()
     {
-        $ast = $this->lexer->readString('((lambda [arg] arg) "executed!")');
-        $result = $this->eval->getReturn($ast);
-        $this->assertEquals('executed!', $result->value());
+        $this->assertEquals(
+            'executed!', $this->valueOf('((lambda [arg] arg) "executed!")'));
 
-        $ast = $this->lexer->readString('((lambda [:x :y] (+ :x :y)) 1 2)');
-        $result = $this->eval->getReturn($ast);
-        $this->assertEquals(3, $result->value());
+        $this->assertEquals(
+            3, $this->valueOf('((lambda [:x :y] (+ :x :y)) 1 2)'));
     }
 
     public function testDefineLambda()
     {
-        $ast = $this->lexer->readString('
-(do
-    (define my-func
-        (lambda [:x :y]
-            (+ :x :y)
-        )
-    )
-    (my-func 1 2)
-)');
-        $result = $this->eval->getReturn($ast);
-        $this->assertEquals(3, $result->value());
+        $this->assertEquals(3, $this->valueOf('
+            (do
+                (define my-func
+                    (lambda [:x :y]
+                        (+ :x :y)
+                    )
+                )
+                (my-func 1 2)
+            )'));
     }
 
     public function testLambdaEvaluatesParams()
     {
-        $ast = $this->lexer->readString('
+        $this->assertEquals(3, $this->valueOf('
             (do
                 (define :x 1)
                 (define :y 2)
                 ((lambda [:num1 :num2]
                     (+ :num1 :num2)
                 ) :x :y)
-            )');
-        $result = $this->eval->getReturn($ast);
-        $this->assertEquals(3, $result->value());
+            )'));
     }
 
     public function testEval()
     {
-        $ast = $this->lexer->readString('(eval :sym)');
-        $result = $this->eval->getReturn($ast);
-        $this->assertInstanceOf('Desmond\\data_types\\SymbolType', $result);
-
-        $ast = $this->lexer->readString('(eval (ast "(+ 1 2)"))');
-        $result = $this->eval->getReturn($ast);
-        $this->assertEquals(3, $result->value());
+        $this->assertInstanceOf(
+            'Desmond\\data_types\\SymbolType', $this->resultOf('(eval :sym)'));
+        $this->assertEquals(3,
+            $this->valueOf('(eval (ast "(+ 1 2)"))'));
     }
 
     public function testLoadFile()
     {
         $this->expectOutputString("30");
-        $ast = $this->lexer->readString('(load-file "' . __DIR__ . '/desmond_files/print-math.dsmnd")');
-        $this->eval->getReturn($ast);
+        $this->resultOf(
+            '(load-file "' . __DIR__ . '/desmond_files/print-math.dsmnd")');
+    }
+
+    private function resultOf($string)
+    {
+        $ast = $this->lexer->readString($string);
+        return $this->eval->getReturn($ast);
+    }
+
+    private function valueOf($string)
+    {
+        return $this->resultOf($string)->value();
     }
 }
