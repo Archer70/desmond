8b7449b3f724261cce6bc5a8f1c1e19df6894cef
Scotty Christianson <code.archer70@gmail.com>
2017-05-07 22:04:05 +0000

    .post function.

---
+++ src/functions/core/DotPost.php
@@ -0,0 +1,28 @@
+<?php
+namespace Desmond\functions\core;
+use Desmond\functions\DesmondFunction;
+use Desmond\data_types\HashType;
+use Desmond\TypeHelper;
+use Exception;
+
+class DotPost implements DesmondFunction
+{
+    use TypeHelper;
+
+    public static function id()
+    {
+        return '.post';
+    }
+
+    public static function run(array $args)
+    {
+        if (!isset($args[0])) {
+            return self::fromPhpType($_POST);
+        }
+        $index = $args[0]->value();
+        if (!isset($_POST[$index])) {
+            throw new Exception('Index "' . $index . '" not found.');
+        }
+        return self::fromPhpType($_POST[$index]);
+    }
+}
