3698c916dd814049b12bf5f13a48f4fb1c7b69b4
Scotty Christianson <code.archer70@gmail.com>
2017-05-14 22:27:48 +0000

    Function generator. May add more features later.

---
+++ bin/funcgen.php
@@ -0,0 +1,47 @@
+#!/bin/php
+<?php
+use Desmond\Desmond;
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$template = '<?php
+namespace Desmond\\functions\\core;
+use Desmond\\functions\\DesmondFunction;
+use Desmond\\ArgumentHelper;
+
+class %s implements DesmondFunction
+{
+    use ArgumentHelper;
+
+    public static function id()
+    {
+        return \'%s\';
+    }
+
+    public static function run(array $args)
+    {
+        $this->expectArguments(
+            \'%s\'
+            [],
+            []
+        );
+        $this->newReturnType(\'\', []);
+    }
+}';
+
+echo "Class name: ";
+$name = str_replace("\n", '', fgets(STDIN));
+
+echo "Identifying symbol: ";
+$id = str_replace("\n", '', fgets(STDIN));
+
+$contents = sprintf($template, $name, $id, $id);
+echo $contents;
+
+echo "\n\n--------------------------------\n\nOkay? [y/n]: ";
+$input = fgets(STDIN);
+if ($input !== "y\n") {
+    exit;
+}
+file_put_contents(__DIR__ . "/../src/functions/core/$name.php", $contents);
+
+echo "Done.\n\n";
