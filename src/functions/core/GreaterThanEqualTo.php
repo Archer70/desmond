036a0f7b647766739b2a73f004291e1bc19e5337
Archer <code.archer70@gmail.com>
2017-09-02 22:53:38 -0700

    Greater than or equal to function.

---
+++ src/functions/core/GreaterThanEqualTo.php
@@ -0,0 +1,32 @@
+<?php
+namespace Desmond\functions\core;
+use Desmond\functions\DesmondFunction;
+use Desmond\ArgumentHelper;
+use Desmond\exceptions\ArgumentException;
+
+class GreaterThanEqualTo extends DesmondFunction
+{
+    use ArgumentHelper;
+
+    public function id()
+    {
+        return '>=';
+    }
+
+    public function run(array $args)
+    {
+        $this->expectArguments('>=', [['Number']], $args);
+        $last = $args[0];
+        array_shift($args);
+        foreach ($args as $number) {
+            if (!$this->isDesmondType('Number', $number)) {
+                throw new ArgumentException('">=" expects arguments to be numbers.');
+            }
+            if ($number->value() > $last->value()) {
+                return $this->newReturnType('False');
+            }
+            $last = $number;
+        }
+        return $this->newReturnType('True');
+    }
+}
