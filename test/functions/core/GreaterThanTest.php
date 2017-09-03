d800b51ded028af5afd3a5e4cc66154c689a352e
Archer <code.archer70@gmail.com>
2017-09-02 22:47:00 -0700

    Greater than function.

---
+++ test/functions/core/GreaterThanTest.php
@@ -0,0 +1,32 @@
+<?php
+namespace Desmond\test\functions\core;
+use PHPUnit\Framework\TestCase;
+use Desmond\test\helpers\RunnerTrait;
+
+class GreaterThanTest extends TestCase
+{
+    use RunnerTrait;
+
+    public function testGreaterThan()
+    {
+        $this->assertTrue($this->valueOf('(> 5 4)'));
+    }
+    
+    public function testNotGreaterThan()
+    {
+		$this->assertFalse($this->valueOf('(> 4 5)'));
+	}
+	
+	public function testMultipleGreaterThan()
+	{
+		$this->assertTrue($this->valueOf('(> 5 4 3 2 1)'));
+	}
+	
+	/**
+	 * @expectedException \Desmond\exceptions\ArgumentException
+	 */
+	public function testNotNumber()
+	{
+		$this->resultOf('(> 5 4 3 2 "one")');
+	}
+}
